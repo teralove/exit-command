b2747bd00301046c499e92f06fab88ee48981b80
teralove <terascripts@gmail.com>
2017-05-05 06:30:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Exit Command
+Exit the game via chat command
+
+### Chat commands:
+!quit
+!exit
+
+Both commands do the exact samething.
+Commands are not case-sensitive.
+
+[slash](https://github.com/baldera-mods/slash) is supported but not required
---
+++ index.js
@@ -0,0 +1,30 @@
+//vers 1.0
+
+const format = require('./format.js');
+
+module.exports = function ExitCommand(dispatch) {
+			
+    dispatch.hook('C_CHAT', 1, function(event) {
+		let command = format.stripTags(event.message).split(' ');
+		
+		if (['!quit', '!exit'].includes(command[0].toLowerCase())) {
+			exitGame();
+			return false;
+		}
+	});	
+	
+	function exitGame() {
+		dispatch.toServer('C_EXIT', 1, {});
+	}
+	
+	// slash support
+	try {
+		const Slash = require('slash')
+		const slash = new Slash(dispatch)
+		slash.on('quit', args => exitGame())
+		slash.on('exit', args => exitGame())
+	} catch (e) {
+		// do nothing because slash is optional
+	}
+
+}
